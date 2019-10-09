303a217410ca715475ae1e38ba44501ad052b34e
CHAHED Nesrine <nesschd@MacBook-Air-de-CHAHED.local>
2019-10-09 11:08:07 +0200

    l

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: nchahed <nchahed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 14:46:42 by nchahed           #+#    #+#             */
-/*   Updated: 2019/10/09 10:56:45 by nchahed          ###   ########.fr       */
+/*   Updated: 2019/10/09 11:07:03 by nchahed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	*ft_calloc(size_t count, size_t size)
 	len = count * size;
 	if (!(tab = malloc(len))
 		return (NULL);
+	ft_memset(tab, 0, len);
 	return (tab);
 	
 }
