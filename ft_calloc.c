191f35cbebcf6dd6c9943cd801c6c1788c72d09f
CHAHED Nesrine <nesschd@MacBook-Air-de-CHAHED.local>
2019-10-09 11:12:31 +0200

    SWP

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: nchahed <nchahed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 14:46:42 by nchahed           #+#    #+#             */
-/*   Updated: 2019/10/09 11:07:03 by nchahed          ###   ########.fr       */
+/*   Updated: 2019/10/09 11:12:15 by nchahed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,10 @@ void	*ft_calloc(size_t count, size_t size)
 	if (!(tab = malloc(len))
 		return (NULL);
 	ft_memset(tab, 0, len);
-	return (tab);
-	
+	return (tab);	
+}
+
+int		main(void)
+{
+	ft
 }
