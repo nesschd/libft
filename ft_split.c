24e81cd5a1bf99edb6a8416233a473391d9a6612
CHAHED Nesrine <nesschd@MacBook-Air-de-CHAHED.local>
2019-10-19 12:06:03 +0200

    split done

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: nchahed <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 10:50:08 by nchahed           #+#    #+#             */
-/*   Updated: 2019/10/17 14:26:29 by nchahed          ###   ########.fr       */
+/*   Updated: 2019/10/19 12:05:43 by nchahed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,10 +40,17 @@ static int	ft_countletter(char const *s, char c)
 	return (letter);
 }
 
-/*
-** EN CAS DE MALLOC QUI FAIL DANS LA BOUCLE IL FAUT FREE
-** TOUS LES MALLOCS PRECEDENTS /!\ /!\ /!\
-*/
+static char	**ft_tabfree(char **tab, int word)
+{
+	word = 0;
+	while (tab[word])
+	{
+		free(tab[word]);
+		word++;
+	}
+	free(tab);
+	return (NULL);
+}
 
 char	**ft_split(char const *s, char c)
 {
@@ -64,35 +71,11 @@ char	**ft_split(char const *s, char c)
 			return (tab);
 		letter = ft_countletter(s, c);
 		if (!(tab[word] = (char *)malloc((letter + 1) * sizeof(char))))
-			return (NULL);
+			return (ft_tabfree(tab, word));
 		ft_strncpy(tab[word], s, letter);
 		tab[word][letter] = '\0';
 		s = s + letter;
 		word++;
 	}
 	return (tab);
-}
-
-/*
-**	LE MAIN DEVRAIT FREE TAB /!\
-*/
-
-int		main(int ac, char **av)
-{
-	char	**tab;
-	int		i;
-
-	if (ac != 3)
-		return (0);
-	if (!(tab = ft_split(av[1], av[2][0])))
-		return (0);
-	i = 0;
-	while (tab[i])
-	{
-		ft_putendl_fd(tab[i], 1);
-		i++;
-	}
-	free (tab[i]);
-	free (tab);
-	return (0);
 }
