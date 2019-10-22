2e54657bd83e0ab89487962a44149ec11b59b2b5
Nesrine CHAHED <nchahed@e1r2p10.42.fr>
2019-10-22 09:45:08 +0200

    done some test

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nchahed <nchahed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 16:00:08 by nchahed           #+#    #+#             */
-/*   Updated: 2019/10/19 16:06:17 by nchahed          ###   ########.fr       */
+/*   Updated: 2019/10/22 09:40:52 by nchahed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,5 +66,6 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char *s, int fd);
 void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
+size_t				ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
